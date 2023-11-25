20ce1704bbd62bd7ed53fb5ea36594150b8e41f9
Calin Daria Andreea <calindariaandreea@gmail.com>
2023-11-25 15:29:08 +0200

    start

--- js/task-07.js
+++
@@ -1,7 +0,0 @@
-const controlSize = document.getElementById("font-size-control");
-const textElement = document.getElementById("text");
-
-controlSize.addEventListener("input", () => {
-  const fontSize = controlSize.value;
-  textElement.style.fontSize = `${fontSize}px`;
-});
